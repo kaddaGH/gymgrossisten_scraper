dd6279ebe680f640a082e3e66e408f644344383a
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-15 11:36:09 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -1,16 +1,8 @@
 module ReqHeaders
   SEARCH_PAGE_HEADER_REQ = {
       'Host' => 'www.gymgrossisten.com',
-      'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-      'accept-encoding' => 'gzip, deflate, br',
-      'accept-language' => 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5',
-      'cache-control' => 'max-age=0',
-      'upgrade-insecure-requests' => '1',
-      'DNT' => '1',
-      'Cookie' => 'RT="dm=www.gymgrossisten.com&si=9fcea9ef-1dc4-4d8d-8385-d33a776e984f&ss=1547302798097&sl=0&tt=0&obo=0&sh=&bcn=%2F%2Fae810539.akstat.io%2F&r=https%3A%2F%2Fwww.gymgrossisten.com%2Fkosttillskott%2Fdrycker%2Fenergidryck%3Fp%3D1&ul=1547304842508&hd=1547304846084"; _gcl_au=1.1.624994320.1547223974; _mtruid=2877947766; tms_VisitorID=l338n97g2s; _ga=GA1.2.1664317400.1547223976; cto_lwid=5cf85305-afc0-403c-9aaf-275945b0a604; cto_idcpy=57d59179-08b3-46f1-bf0e-323d2dcebc35; __zlcmid=qJhvyQH9IPHquL; frontend=3nv3hlgch594vkad97a1p5lii0',
-
-      'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-  }
+      'User-Agent' => 'Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)',
+      'Accept' => '*/*', }
 
 end
 
