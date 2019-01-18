7c80d2cc032ed464ad658ada7de5d11e8ae83892
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-18 21:01:40 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,22 +1,16 @@
 require './lib/headers'
 
 # 1: Seed taxonomy & search pages
-
-search_terms = [   'Energidrycker']
-search_terms.take(1).each do |search_term|
-
-  pages << {
-      page_type: 'products_listing',
-      method: 'GET',
-      headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
-      url: "https://www.gymgrossisten.com/catalogsearch/result/?q=#{search_term.gsub(' ', '+')}",
-      vars: {
-          'input_type' => 'search',
-          'search_term' => search_term,
-          'page' => 1
-      }
+pages << {
+    page_type: 'products_listing',
+    method: 'GET',
+    headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
+    url: "https://www.gymgrossisten.com/kosttillskott/drycker/energidryck",
+    vars: {
+        'input_type' => 'taxonomy',
+        'search_term' => '-',
+        'page' => 1
+    }
 
 
-  }
-
-end
+}
