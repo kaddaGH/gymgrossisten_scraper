ee38a97a5a19b530c667f07b4c4305b7012c449e
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-18 21:38:55 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -14,3 +14,21 @@ pages << {
 
 
 }
+search_terms = ['Red Bull', 'RedBull', 'Energidryck', 'Energidrycker']
+search_terms.each do |search_term|
+
+  pages << {
+      page_type: 'products_listing',
+      method: 'GET',
+      headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
+      url: "https://www.gymgrossisten.com/catalogsearch/result/?q=#{search_term.gsub(' ', '+')}",
+      vars: {
+          'input_type' => 'search',
+          'search_term' => search_term,
+          'page' => 1
+      }
+
+
+  }
+
+end
