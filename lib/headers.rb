a203051cdca03f3f04df16ad81d52dab766c412a
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-18 20:10:20 +0100

    Update header.

--- lib/headers.rb
+++ lib/headers.rb
@@ -2,7 +2,11 @@ module ReqHeaders
   SEARCH_PAGE_HEADER_REQ = {
       'Host' => 'www.gymgrossisten.com',
       'User-Agent' => 'Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)',
-      'Accept' => '*/*', }
+      'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'accept-encoding' => 'gzip, deflate, br'
+
+
+  }
 
 end
 
