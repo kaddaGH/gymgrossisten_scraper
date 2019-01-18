2e8cf53f61863e0e33aa672c6443092318d9562f
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-18 20:30:51 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,21 +1,9 @@
 require './lib/headers'
 
 # 1: Seed taxonomy & search pages
-pages << {
-    page_type: 'products_listing',
-    method: 'GET',
-    headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
-    url: "https://www.gymgrossisten.com/kosttillskott/drycker/energidryck",
-    vars: {
-        'input_type' => 'taxonomy',
-        'search_term' => '-',
-        'page' => 1
-    }
 
-
-}
 search_terms = ['Red Bull', 'RedBull', 'Energidryck', 'Energidrycker']
-search_terms.each do |search_term|
+search_terms.take(1).each do |search_term|
 
   pages << {
       page_type: 'products_listing',
