6751f616322464b3629447ba86ee86e835c84972
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-20 15:16:31 +0100

    Update scraper.

--- parsers/products_listing.rb
+++ parsers/products_listing.rb
@@ -5,14 +5,14 @@ products = body.css(".product-list-item")
 scrape_url_nbr_products = body.at_css(".amount").text.strip.split("av").last.strip.to_i
 
 
-products.each_with_index do |product,i|
+products.each_with_index do |product, i|
 
   pages << {
       page_type: 'product_details',
       method: 'GET',
       headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
-      url: product.at(".product-name").at("a").attr("href")+"?search=#{page['vars']['search_term']}",
-      vars:{
+      url: product.at(".product-name").at("a").attr("href") + "?search=#{page['vars']['search_term']}+&rank=#{i + 1}",
+      vars: {
           'input_type' => page['vars']['input_type'],
           'search_term' => page['vars']['search_term'],
           'SCRAPE_URL_NBR_PRODUCTS' => scrape_url_nbr_products,
@@ -20,7 +20,7 @@ products.each_with_index do |product,i|
           'page' => page['vars']['page']
       }
 
-      }
+  }
 
 end
 
